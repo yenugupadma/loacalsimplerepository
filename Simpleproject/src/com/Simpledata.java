e68d77e05ab1ca0f9b2d9c7c20fea2280c669209
padmavathi <yenugupadmavathi@gmail.com>
2017-08-27 23:06:10 +0530

    intiated simple output

---
+++ Simpleproject/src/com/Simpledata.java
@@ -0,0 +1,24 @@
+package com;
+
+public class Simpledata extends Extradata {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Extradata ed=new Extradata();
+		
+		displayData();
+		ed.display();
+		
+
+	}
+
+	private static void displayData() {
+		// TODO Auto-generated method stub
+		System.out.println("HI");
+		System.out.println("how R U");
+		System.out.println("rackspace");
+		
+	}
+	
+
+}
